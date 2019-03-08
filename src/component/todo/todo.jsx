514ae25b5780b4103421b768f9f37e737dbcbf5a
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-08 17:27:34 +0500

    Create: :construction: UI of todos

--- src/component/todo/todo.jsx
+++ src/component/todo/todo.jsx
@@ -9,21 +9,23 @@ class Todo extends Component {
   }
   render() {
     return (
-      <div className="todo">
-        <div className="todo-header">
-          <div className="title">Title</div>
-          <div className="priority">MG</div>
-        </div>
-        <div className="todo-section">
-          <div className="content">
-            Lorem ipsum dolor sit amet consectetur adipisicing...
+      <div className="todo flex-container">
+        <div className="todo-header item flex-container">
+          <div className="title item">
+            <h4>Title</h4>
           </div>
-          <div className="meta">date</div>
+          <div className="priority item">MG</div>
         </div>
-        <div className="todo-action">
-          <button>Done</button>
-          <button>InProgress</button>
-          <button>Delete</button>
+        <div className="todo-section item flex-container">
+          <div className="content item">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing...</p>
+          </div>
+          <span className="date item">10 days ago</span>
+        </div>
+        <div className="todo-action item flex-container">
+          <button className="item btn blue">Done</button>
+          <button className="item btn purple">InProgress</button>
+          <button className="item btn grey">Delete</button>
         </div>
       </div>
     );
